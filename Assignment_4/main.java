13ff88b637584fcf68128af8bd497d0bd0474cd1
grvpr03 <gaurav.prakash.03@gmail.com>
2024-05-01 12:28:22 +0530

    done assignment 4

---
+++ Assignment_4/main.java
@@ -0,0 +1,10 @@
+package Assignment_4;
+
+public class main {
+
+	    public static void main(String[] args) {
+	        Deck deck = new Deck();
+	        deck.createDeck(); // Create a deck of cards
+	        deck.displayMenu(); // Display the menu for the user
+	    }
+	}
